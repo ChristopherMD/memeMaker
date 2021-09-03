d36e3e9cc46d83798e0b10fb098fc7c84f8a330b
Christopher Doud <rhinowrkshp@gmail.com>
2021-09-02 22:37:35 -0500

    Add files via upload

---
+++ App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Header from './Header';
+import MemeGenerator from './MemeGenerator';
+
+function App() {
+  return (
+    <div>
+      <h1>Hello world!</h1>
+      <Header />
+      <MemeGenerator />
+    </div>
+  );
+}
+
+export default App;
